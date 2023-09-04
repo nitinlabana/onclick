d2ea509ca5309ea2356746427ece0f424bc92a49
nitinlabana <67837026+nitinlabana@users.noreply.github.com>
2023-09-04 12:46:12 +0530

    chnage folder

---
+++ index.js
@@ -0,0 +1,38 @@
+function red() {
+    document.getElementById("content").style.backgroundColor = "red";
+    document.getElementById("navbar").style.backgroundColor = "rgb(2,0,36);";
+    document.getElementById("hero").style.backgroundColor = "red";
+
+}
+function orange() {
+    document.getElementById("content").style.backgroundColor = "orange";
+    document.getElementById("navbar").style.backgroundColor = "pink";
+    document.getElementById("hero").style.backgroundColor = "orange";
+
+}
+function yellow() {
+    document.getElementById("content").style.backgroundColor = "yellow";
+    document.getElementById("navbar").style.backgroundColor = "red";
+    document.getElementById("hero").style.backgroundColor = "yellow";
+}
+function green() {
+    document.getElementById("content").style.backgroundColor = "green";
+    document.getElementById("navbar").style.backgroundColor = "white";
+    document.getElementById("hero").style.backgroundColor = "green";
+}
+function blue() {
+    document.getElementById("content").style.backgroundColor = "blue";
+    document.getElementById("navbar").style.backgroundColor = "indigo";
+    document.getElementById("hero").style.backgroundColor = "blue";
+}
+function indigo() {
+    document.getElementById("content").style.backgroundColor = "indigo";
+    document.getElementById("navbar").style.backgroundColor = "black";
+    document.getElementById("hero").style.backgroundColor = "indigo";
+}
+function violet() {
+    debugger;
+    document.getElementById("content").style.backgroundColor = "violet";
+    document.getElementById("navbar").style.backgroundColor = "yellow";
+    document.getElementById("hero").style.backgroundColor = "violet";
+}
